aad4c81ea63a7c469e05fe25c1014edd004ba99b
chinna vadada <chinna.vadada@klm.com>
2015-11-28 23:08:19 +0100

    created encrypt/decrypt utility component.

--- VolunteerSystem/src/main/java/org/bluecross/util/EMailUtility.java
+++ VolunteerSystem/src/main/java/org/bluecross/util/EMailUtility.java
@@ -10,6 +10,8 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -19,53 +21,54 @@ import org.springframework.stereotype.Component;
 @Component
 public class EMailUtility {
 
-	@Value("#{mail.username}")
-	private String username;
+    private final Logger LOGGER = LoggerFactory.getLogger(EMailUtility.class);
 
-	@Value("#{mail.password}")
-	private String password;
+    @Value("#{mail.username}")
+    private String username;
 
-	@Value("#{mail.smtp.host}")
-	private String smtpHost;
+    @Value("#{mail.password}")
+    private String password;
 
-	@Value("#{mail.smtp.port}")
-	private String smtpPort;
+    @Value("#{mail.smtp.host}")
+    private String smtpHost;
 
-	private Session session = null;
+    @Value("#{mail.smtp.port}")
+    private String smtpPort;
 
-	public EMailUtility() {
-		Properties props = new Properties();
-		props.put("mail.smtp.host", smtpHost);
-		props.put("mail.smtp.socketFactory.port", smtpPort);
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", smtpPort);
+    private Session session = null;
 
-		session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
-			}
-		});
-	}
+    public EMailUtility() {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.socketFactory.port", smtpPort);
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", smtpPort);
 
-	/**
-	 * @param emailId,
-	 *            email To recipient address
-	 * @param subject,
-	 *            email subject
-	 * @param body,
-	 *            email body
-	 */
-	public void sendEmail(String emailId, String subject, String body) {
-		try {
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(username));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailId));
-			message.setSubject(subject);
-			message.setText(body);
-			Transport.send(message);
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+    }
+
+    /**
+     * @param emailId, email To recipient address
+     * @param subject, email subject
+     * @param body, email body
+     */
+    public void sendEmail(String emailId, String subject, String body) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailId));
+            message.setSubject(subject);
+            message.setText(body);
+            Transport.send(message);
+            LOGGER.info("Successfully sent an email to " + emailId);
+        }
+        catch (MessagingException e) {
+            LOGGER.error("Exception occured while sending an email: " + e.getMessage());
+        }
+    }
 }
