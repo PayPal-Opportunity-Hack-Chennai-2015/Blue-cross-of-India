c7fa8d351c87b1dc31036c217d3a44bf9f47a629
Varun Jayanti <varun.jayanti@klm.com>
2015-11-29 01:00:21 +0530

    REST Save operation for volunteer

--- VolunteerSystem/src/main/java/org/bluecross/service/VolunteerService.java
+++ VolunteerSystem/src/main/java/org/bluecross/service/VolunteerService.java
@@ -3,6 +3,9 @@
  */
 package org.bluecross.service;
 
+import java.util.Map;
+
+import org.bluecross.persistence.data.Volunteer;
 import org.bluecross.repository.persistence.VolunteerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,5 +23,10 @@ public class VolunteerService {
 	public String getName() {
 		return String.valueOf(volunteerRepository.findAll().spliterator().getExactSizeIfKnown());
 	}
+
+	public String save(Volunteer volunteer) {
+		return String.valueOf(volunteerRepository.save(volunteer).getUserId());
+	}
+
 
 }
--- VolunteerSystem/src/main/java/org/bluecross/volunteer/controller/VolunteerController.java
+++ VolunteerSystem/src/main/java/org/bluecross/volunteer/controller/VolunteerController.java
@@ -3,8 +3,12 @@
  */
 package org.bluecross.volunteer.controller;
 
+import java.util.Map;
+
+import org.bluecross.persistence.data.Volunteer;
 import org.bluecross.service.VolunteerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +23,13 @@ public class VolunteerController {
 	@Autowired
 	private VolunteerService volunteerService;
 	
-    @RequestMapping("/boot/user")
-    public String index() {
-        return volunteerService.getName();
+    /**
+     * @param volunteer
+     * @return
+     */
+    @RequestMapping("/add")
+    public String save(@RequestBody Volunteer volunteer) {
+        return volunteerService.save(volunteer);
     }
 
 }
