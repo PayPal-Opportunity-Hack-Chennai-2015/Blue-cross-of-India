537bb7c70d13949ab07c63dd0a1b0b7ff9967e7a
rakesh <ragavlura@gmail.com>
2015-11-28 15:05:35 +0530

    AddedUserModel

--- WEB-APP/models/index.js
+++ WEB-APP/models/index.js
@@ -6,10 +6,35 @@ var Schema       = mongoose.Schema;
 var animalSchema = mongoose.Schema({
 
     name             : String,
-    location         : String
+    subcategory  : []
 
 });
 
+var userSchema = mongoose.Schema( {
+
+	username:{ type: String, required: true, trim: true, index: { unique: true } },
+	password:{ type: String, required: true },
+	email: { type:String, required: true, trim: true },
+	phoneNumber:{ type:String, required: true, trim: true },
+	firstName:{ type:String, required: true, trim: true},
+	lastName:{ type:String, required: true, trim: true}
+});
+
+var animalRescueSchema = mongoose.Schema({
+
+	name:{ type: String, required: true, trim: true },
+	type:{ type: String, required: true, trim: true},
+	location: [],
+	state: [],
+	treatment: [],
+	doctor: { type: String, required: true, trim: true },
+	pic:   String,
+	assignedTo: String 
+})
+
+
 module.exports = {
-	Animal : mongoose.model('Animal', animalSchema)
+	Animal : mongoose.model('Animal', animalSchema),
+	User : mongoose.model('User', userSchema),
+	AnimalRescue  : mongoose.model('AnimalRescue', animalRescueSchema)
 }
