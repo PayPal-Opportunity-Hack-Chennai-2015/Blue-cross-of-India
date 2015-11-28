63096ce0d8fe0ab1229092b4624d336e2f00a16b
manaswini challa <manaswini.challa@tcs.com>
2015-11-29 04:17:35 +0530

    BCI: Added POST method type to controller

--- VolunteerSystem/src/main/java/org/bluecross/volunteer/controller/VolunteerController.java
+++ VolunteerSystem/src/main/java/org/bluecross/volunteer/controller/VolunteerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,7 +30,7 @@ public class VolunteerController {
      * @param volunteer
      * @return
      */
-    @RequestMapping("/add")
+    @RequestMapping(value="/add",method=RequestMethod.POST)
     public String save(@RequestBody Volunteer volunteer) {
         return volunteerService.save(volunteer);
     }
